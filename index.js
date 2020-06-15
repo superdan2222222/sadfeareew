4c31c21a32389958439a8afa037779ab31bf437b
Dan Golkd <dgoldring27@gmail.com>
2020-06-15 16:59:29 -0400

    Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const discord = require('discord.js');
+const client = new discord.Client();
+
+client.on('ready', () => {
+    console.log('Logged in')
+});
+
+client.login('NzEzMDc4NjEwNDc4NDk3OTM5.Xufglw.Ti3InmwaTYa-grwKr8urvD_3L_Y')
